1cfaabe454e043b0ea491f177fddf61bedb6177e
Viktor Fonic <viktor.fonic@gmail.com>
2019-02-06 13:00:21 -0500

    Broken version

--- db/migrate/20190206174400_add_boolean_column_to_new_tables.rb
+++ db/migrate/20190206174400_add_boolean_column_to_new_tables.rb
@@ -1,5 +1,5 @@
 class AddBooleanColumnToNewTables < ActiveRecord::Migration[5.2]
   def change
-    # add_column :new_tables, :boolean_column, :boolean, default: false
+    add_column :new_tables, :boolean_column, :boolean, default: false
   end
 end
